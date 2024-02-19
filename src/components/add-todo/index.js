fabf8bb0cb4182b008dc9cf56e9f34ffa9260da8
ednaakpene <edvalerie18@gmail.com>
2024-02-19 13:51:25 +0000

    first commit

---
+++ src/components/add-todo/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./index.module.css";
+function AddTodo() {
+    // const [todo, setTodo] = React.useState("");
+    let todo;
+    function collectInput(event) {
+        // console.log(event.target.value);
+        todo = event.target.value;
+    }
+    function saveTodo() {
+        console.log(todo);
+    }
+    return (
+        <section className={styles.addTodo}>
+            <input
+                onChange={collectInput}
+                className={styles.addTodoInput}
+                placeholder="Start typing..." />
+            <button onClick={saveTodo}>Create</button>
+        </section>
+    );
+}
+
+export default AddTodo;
---
+++ src/components/todo-list/index.js
@@ -0,0 +1,16 @@
+function TodoList() {
+    return (
+        <ul>
+            <li>Complete online javascript course</li>
+            <li>eat three times a day</li>
+            <li>play football</li>
+            <li>buy a house</li>
+            <li>eat banana</li>
+            <li>learn css</li>
+            <li>Jog around the field twice</li>
+
+        </ul>
+    )
+}
+
+export default TodoList;
