c28938834393e8d303abc3c586e7f8b9624b7da0
ednaakpene <edvalerie18@gmail.com>
2024-02-20 14:11:53 +0000

    refactored code

--- src/components/add-todo/index.js
+++ src/components/add-todo/index.js
@@ -1,14 +1,37 @@
-import React from "react";
+// import React from "react";
 import styles from "./index.module.css";
+import { useLocalStorage } from "usehooks-ts";
+import { useState } from "react";
+
 function AddTodo() {
     // const [todo, setTodo] = React.useState("");
-    let todo;
+    const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
+    // let todo;
+
+    // todos is the old one  todo is the new one the person is typing
+
+    const [todo, setTodo] = useState("");
+
     function collectInput(event) {
         // console.log(event.target.value);
-        todo = event.target.value;
+        // todo = event.target.value;
+
+        setTodo(event.target.value);
     }
+
+    // arrow function
+    // const collectInput = (event) => { setTodo(event.target.value); }
+
     function saveTodo() {
-        console.log(todo);
+        // console.log(todo);
+        // get existing list of todos from local storage
+        // let todos = JSON.parse(localStorage.getItem("TODO_KEY")) || [];
+
+        // add new todo to existing list of todos 
+        // todos.push(todo);
+        // Set all todos in local storage
+        setTodos([...todos, todo]);
+        // TODO_KEY is the key todo is the value
     }
     return (
         <section className={styles.addTodo}>
@@ -20,5 +43,4 @@ function AddTodo() {
         </section>
     );
 }
-
 export default AddTodo;
--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,16 +1,26 @@
+// import { useState, useEffect } from "react";
+import { useLocalStorage } from "usehooks-ts";
+
 function TodoList() {
+
+    // let todos = [];
+    const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
+    // function getTodos() {
+    //     // get all todos from local storage and store it
+    //     let todos = JSON.parse(localStorage.getItem("TODO_KEY")) || [];
+
+    //     // update react state
+    //     setTodos(todos);
+
+    // }
+    // useEffect(getTodos, []);
     return (
         <ul>
-            <li>Complete online javascript course</li>
-            <li>eat three times a day</li>
-            <li>play football</li>
-            <li>buy a house</li>
-            <li>eat banana</li>
-            <li>learn css</li>
-            <li>Jog around the field twice</li>
-
+            {todos.map(function (todo, index) {
+                return <li key={index}>{todo}</li>
+            })}
         </ul>
-    )
+    );
 }
 
 export default TodoList;
